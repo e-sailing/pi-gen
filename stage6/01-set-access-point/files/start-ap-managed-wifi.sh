365cca764f81832ae698fb68e5664bf25a0b6e9d
e-sailing <martin.stell@freenet.de>
2019-06-28 11:14:53 +0200

    test network default

--- stage6/01-set-access-point/files/start-ap-managed-wifi.sh
+++ stage6/01-set-access-point/files/start-ap-managed-wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 internet=wlan0
-router=wlan1
+router=wlan9
 sudo sysctl -w net.ipv4.ip_forward=1
 /bin/bash /home/pi/.openplotter/start1.sh
 /bin/bash /home/pi/.openplotter/iptables.sh "$internet" "$router"
