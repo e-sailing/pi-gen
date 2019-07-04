0b5f16a8c0057aebf845d25b64f6abc1c759a97b
Sailoog <info@sailoog.com>
2019-07-04 18:58:13 +0200

    fix sdr

--- stage8/02-install-sdr/01-run.sh
+++ stage8/02-install-sdr/01-run.sh
@@ -1,20 +1,18 @@
 #!/bin/bash -e
 
-#install gqrx
 on_chroot << EOF
-cd /opt
-rm -f gqrx-sdr-2.11.5-linux-rpi3.tar.xz
-rm -rf gqrx
-rm -rf gqrx-sdr-2.11.5-linux-rpi3
-wget https://github.com/csete/gqrx/releases/download/v2.11.5/gqrx-sdr-2.11.5-linux-rpi3.tar.xz
-tar xf gqrx-sdr-2.11.5-linux-rpi3.tar.xz
-rm -f gqrx-sdr-2.11.5-linux-rpi3.tar.xz
-mv gqrx-sdr-2.11.5-linux-rpi3 gqrx
-cd gqrx
-cp udev/*.rules /etc/udev/rules.d/
+sudo -u pi pulseaudio --start
 EOF
 
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.config/gqrx"
 install -m 644 -o 1000 -g 1000 files/default.conf		"${ROOTFS_DIR}/home/pi/.config/gqrx/"
 install -m 644 -o 1000 -g 1000 files/bookmarks.csv		"${ROOTFS_DIR}/home/pi/.config/gqrx/"
-rm -f "${ROOTFS_DIR}/usr/share/applications/gnuradio-grc.desktop"
+install -m 644 files/udev/52-airspy.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/udev/52-airspyhf.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/udev/53-hackrf.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/udev/66-mirics.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/udev/fcd.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/udev/fcdpp.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/udev/rtl-sdr.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+install -m 644 files/udev/sdriq.rules		"${ROOTFS_DIR}/etc/udev/rules.d/"
+rm -f "${ROOTFS_DIR}/usr/share/applications/gqrx.desktop"
